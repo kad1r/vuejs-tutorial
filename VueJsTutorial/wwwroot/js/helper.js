2e7aee9f5d0f3e648c555edc8b15605f1fc44a6c
kadir.avci <kadir.avci@cts.com.tr>
2019-08-09 17:38:15 +0300

    Changed site.css and site.js. Added helper.js. Added bootstrap cdn to layout.cshtml.

---
+++ VueJsTutorial/wwwroot/js/helper.js
@@ -0,0 +1,27 @@
+﻿"use strict"
+
+function $get(ref, url) {
+	axios.get(url)
+		.then(response => {
+			//ref.result = response.data.result
+			ref.result = response.data
+		})
+		.catch(error => console.log(error));
+}
+
+function ajaxcall(url, method) {
+	var request = new httpRequest();
+
+	request.method = method;
+	request.url = url;
+
+	request.success = function (response) {
+		return JSON.parse(JSON.stringify(response))
+	};
+
+	request.fail = function (error) {
+		console.log(error);
+	};
+
+	request.send();
+};
